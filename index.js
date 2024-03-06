434c48fa6298ea2d5a555e970b62b59f209cdff5
jujohiro <you@example.com>
2024-03-05 21:06:37 -0500

    combinacion de dos arrays

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-var cadena = "Hola, mundo, cómo, estás";
-var arrayDesdeCadena = cadena.split(", ");
-console.log(arrayDesdeCadena);
+var array1 = [1, 2, 3];
+var array2 = [4, 5, 6];
+var arrayCombinado = [...array1, ...array2];
+console.log(arrayCombinado);
