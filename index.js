96eeac486e71dec1bf626cff6dd3dd6ab7ea61d9
jujohiro <you@example.com>
2024-03-05 22:38:44 -0500

    se crea rama para convertir array

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var array = [1, 2, 3, 4, 5];
-var porcion = array.slice(1, 3);
-console.log(porcion);
+var array = ["Hola", "mundo", "cómo", "estás"];
+var string = array.join(" ");
+console.log(string);
