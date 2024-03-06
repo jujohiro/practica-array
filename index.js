27e81c9f3bb2da17982361a128b7a2897d1ab69a
jujohiro <you@example.com>
2024-03-05 22:35:28 -0500

    obtener una porcion del array

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 var array = [1, 2, 3, 4, 5];
-var primerElemento = array.shift();
-console.log(primerElemento); // Output: 1
-console.log(array);
+var porcion = array.slice(1, 3);
+console.log(porcion);
